b2c8282834be3f693648abc2a2b066225e939d72
jwkim409 <122677237+jwkim409@users.noreply.github.com>
2023-03-10 12:12:09 +0900

    Update hello.py

--- 혼공_Python/hello.py
+++ 혼공_Python/hello.py
@@ -3,3 +3,13 @@ n = int(input('number : '))
 for i in range(1, n+1):
 
     print(" "*(n-i), "*"*(2*i-1))
+    
+    
+# while
+list_test = [1,2,1,2]
+value = 2
+
+while value in list_test:
+    list_test.remove(value)
+
+print(list_test)
