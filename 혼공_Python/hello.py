1f89dd8d80fd3ab8b22d71015dd852009d6a4a14
jwkim409 <122677237+jwkim409@users.noreply.github.com>
2023-03-12 13:03:19 +0900

    Update hello.py

--- 혼공_Python/hello.py
+++ 혼공_Python/hello.py
@@ -13,3 +13,18 @@ while value in list_test:
     list_test.remove(value)
 
 print(list_test)
+
+
+# 정수 더하기
+def sum_all(start, end):
+    output = 0
+
+    # range는 (끝 숫자 - 1) 을 하기 때문에 +1을 해줘야
+    for i in range(start, end + 1):
+        output += i
+    return output
+
+print(sum_all(0, 100))
+print(sum_all(0, 1000))
+print(sum_all(50, 100))
+print(sum_all(50, 100))
